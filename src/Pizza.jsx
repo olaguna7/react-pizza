765595caf4686bb1a52598b64d446f4339d7267e
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-09 11:02:26 +0200

    Add initial components for app

---
+++ src/Pizza.jsx
@@ -0,0 +1,13 @@
+const Pizza = (props) => {
+    return(
+        <div className="pizza" onClick={() => {
+            console.log("hi")
+        }}>
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <img src={props.image} alt={props.name} />
+        </div>
+    );
+};
+
+export default Pizza;
