b1d902a0f128d6bdd4ddd23c7b67906f2c67902f
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-10 17:38:40 +0200

    Add tests for custom hook

---
+++ src/__tests__/usePizzaOfTheDay.test.jsx
@@ -0,0 +1,23 @@
+import { expect, test, vi } from "vitest";
+import { renderHook } from "@testing-library/react";
+import createFetchMock from "vitest-fetch-mock";
+import { usePizzaOfTheDay } from "../usePizzaOfTheDay";
+
+const fetchMocker = createFetchMock(vi);
+fetchMocker.enableMocks();
+
+const testPizza = {
+    id: "calabrese",
+    name: "The Calabrese Pizza",
+    category: "Supreme",
+    description: "lol pizza from Calabria",
+    image: "/public/pizzas/calabrese.webp",
+    size: { S: 12.25, M: 16.25, L: 20.25 },
+};
+
+
+test("gives null when first called", async () => {
+    fetch.mockResponseOnce(JSON.stringify(testPizza));
+    const { result } = renderHook(() => usePizzaOfTheDay());
+    expect(result.current).toBeNull();
+})
