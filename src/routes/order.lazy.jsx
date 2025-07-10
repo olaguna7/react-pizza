513f0b4c7015e55e24954832037bb8cb1044b998
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-10 18:59:43 +0200

    Add form actions features from react 19

--- src/routes/order.lazy.jsx
+++ src/routes/order.lazy.jsx
@@ -56,19 +56,18 @@ function Order() {
     setLoading(false);
   }
 
+  function addToCart() {
+    setCart([
+              ...cart,
+              { pizza: selectedPizza, size: pizzaSize, price },
+            ]);
+  }
+
   return (
     <div className="order-page">
       <div className="order">
         <h2>Create Order</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setCart([
-              ...cart,
-              { pizza: selectedPizza, size: pizzaSize, price },
-            ]);
-          }}
-        >
+        <form action={addToCart}>
           <div>
             <div>
               <label htmlFor="pizza-type">Pizza Type</label>
